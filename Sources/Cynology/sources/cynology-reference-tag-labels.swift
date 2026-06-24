b26a6406f7182d6441d6ccb27f3bab1c488d379c
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2026-06-24 13:02:11 +0200

    src tags

---
+++ Sources/Cynology/sources/cynology-reference-tag-labels.swift
@@ -0,0 +1,51 @@
+public enum CynologyReferenceTagLabels {
+    public static let dutch: [String: String] = [
+        CynologyTag.learning_theory.tag.id: "Leertheorie",
+        CynologyTag.classical_conditioning.tag.id: "Klassieke conditionering",
+        CynologyTag.operant_conditioning.tag.id: "Operante conditionering",
+        CynologyTag.reinforcement.tag.id: "Bekrachtiging",
+        CynologyTag.punishment.tag.id: "Bestraffing",
+        CynologyTag.matching_law.tag.id: "\"Matching Law\"",
+        CynologyTag.action_habit_control.tag.id: "Actie- en gewoonte",
+        CynologyTag.context_renewal.tag.id: "Vernieuwing",
+        CynologyTag.engagement.tag.id: "Betrokkenheid",
+
+        CynologyTag.affective_state.tag.id: "Affectieve toestand (gemoed)",
+        CynologyTag.motivation.tag.id: "Motivatie",
+        CynologyTag.frustration_tolerance.tag.id: "Frustratie",
+        CynologyTag.resilience.tag.id: "Weerbaarheid",
+        CynologyTag.challenge_calibration.tag.id: "Uitdaging (analoog)",
+
+        CynologyTag.training_methods.tag.id: "Trainingsmethoden",
+        CynologyTag.behavior_modification.tag.id: "Gedragsmodificatie",
+        CynologyTag.clinical_behavior.tag.id: "Klinisch",
+        CynologyTag.methodology.tag.id: "Methodologie",
+        CynologyTag.assessment.tag.id: "Beoordeling",
+        CynologyTag.temperament.tag.id: "Temperament",
+
+        CynologyTag.problem_behavior.tag.id: "Probleemgedrag",
+        CynologyTag.reactivity.tag.id: "Reactiviteit",
+        CynologyTag.aggression.tag.id: "Agressie",
+        CynologyTag.anxiety.tag.id: "Angst",
+        CynologyTag.separation_distress.tag.id: "Verlatingsangst",
+        CynologyTag.noise_sensitivity.tag.id: "Geluidsfobie",
+
+        CynologyTag.stress.tag.id: "Stress",
+        CynologyTag.welfare.tag.id: "Welzijn",
+        CynologyTag.physiology.tag.id: "Fysiologie",
+        CynologyTag.pain_medical.tag.id: "Pijn",
+        CynologyTag.aversives.tag.id: "Aversieven",
+        CynologyTag.electronic_training_devices.tag.id: "Elektronische trainingsmiddelen",
+
+        CynologyTag.communication.tag.id: "Communicatie",
+        CynologyTag.canine_body_language.tag.id: "Lichaamstaal",
+        CynologyTag.socialization.tag.id: "Socialisatie",
+        CynologyTag.canine_behavior.tag.id: "Hondengedrag",
+        CynologyTag.play.tag.id: "Spel",
+        CynologyTag.dominance.tag.id: "Dominantie",
+        CynologyTag.human_dog_relationship.tag.id: "Mens-hondrelatie",
+
+        CynologyTag.diet.tag.id: "Voeding",
+        CynologyTag.working_dogs.tag.id: "Werkhonden"
+    ]
+}
